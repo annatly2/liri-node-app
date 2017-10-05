c16cc8b38454e41c3b13eb16789852df87c3ac9a
Anna Ly <annatly2@gmail.com>
2017-10-04 19:13:23 -0700

    oct 4

--- liri.js
+++ liri.js
@@ -1,33 +1,65 @@
 var theKeys = require("./keys.js");
-
-console.log("Hello there!");
-
 var command = process.argv[2];
 
+switch (command){
+	case "my-tweets":
+		getTweets();
+		break;
+
+	case "spotify-this-song":
+		spotifySong();
+		break;
+
+	case "movie-this":
+		movieThis();
+		break;
+
+	case "do-what-it-says":
+		doWhat();
+		break;
+}
+
+function getTweets(){
+		console.log("gonna get the tweets");
+
+}
+
+function spotifySong(){
 
 
+}
 
-if (command === "my-tweets"){
-	var queryURL = "https://api.twitter.com/1.1/statuses/user_timeline.json?screen_name=twitterapi&count=2"
+function movieThis(){
+	var request = require("request");
+	var movieArg = process.argv;
+	var movieTitle = "";
 
-	$.ajax({
-		url: queryURL,
-		method: "GET"
-	}).done(function(response){
-		console.log(response);
-	})
-	console.log("My tweets are here!");
+	for (var i = 3; i < movieArg.length; i++){
+		if (i > 3 && i < movieArg.length){
+			movieTitle = movieTitle + "+" + movieArg[i];
+		}else{
+			movieTitle += movieArg[i];
+		}
+	}	
 
+	var theOMBDKey = theKeys.ombdKey.apiKey;
+	var queryURL = "http://www.omdbapi.com/?t=" + movieTitle + "&y=&plot=short&apikey="+theOMBDKey;
+	console.log(queryURL);
 
-// //show last 20 tweets and when they were created in terminal
- }
- //else if(command === "spotify-this-song"){
-// 	//show artist(s), song's name, preview of link to the song from spotify, and album that song is from
-// }
+	request(queryURL, function(error, response, body){
+		if(!error && response.statusCode === 200){
+			console.log("Title: " + JSON.parse(body).Title);
+			console.log("Year: " + JSON.parse(body).Year);
+			console.log("IMDB Rating: "+ JSON.parse(body).imdbRating);
+			console.log("Rotten Tomatoes Rating: "+ JSON.parse(body).Ratings[1].Value);
+			console.log("Country: " + JSON.parse(body).Country);
+			console.log("Language: " + JSON.parse(body).Language);
+			console.log("Plot: " + JSON.parse(body).Plot);
+			console.log("Actors: " + JSON.parse(body).Actors);
+		}
+	});
+}
 
+function doWhat(){
 
-
-
-// "movie-this"
-
-// "do-what-it-says"
+}
